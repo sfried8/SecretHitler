8b7ffc42f7234584df32051a6e6631cf493eb6b1
Sam <sfried8@gmail.com>
2017-12-22 13:01:35 -0500

    blah

--- src/Enums.js
+++ src/Enums.js
@@ -8,7 +8,7 @@ const Executive_Action =
     };
 const Setups = {
     3: {Liberals: 1, Fascists: 1, hitlerKnowsFascists: true, board:[Executive_Action.NoAction, Executive_Action.NoAction, Executive_Action.PolicyPeek, Executive_Action.Execution, Executive_Action.Execution] },
-    5: {Liberals: 3, Fascists: 1, hitlerKnowsFascists: true, board:[Executive_Action.NoAction, Executive_Action.NoAction, Executive_Action.PolicyPeek, Executive_Action.SpecialElection, Executive_Action.Execution]},
+    5: {Liberals: 3, Fascists: 1, hitlerKnowsFascists: true, board:[Executive_Action.Execution, Executive_Action.NoAction, Executive_Action.PolicyPeek, Executive_Action.SpecialElection, Executive_Action.Execution]},
     6: {Liberals: 4, Fascists: 1, hitlerKnowsFascists: true, board:[Executive_Action.NoAction, Executive_Action.NoAction, Executive_Action.PolicyPeek, Executive_Action.Execution, Executive_Action.Execution]},
     7: {Liberals: 4, Fascists: 2, hitlerKnowsFascists: false, board:[Executive_Action.NoAction, Executive_Action.InvestigateLoyalty, Executive_Action.SpecialElection, Executive_Action.Execution, Executive_Action.Execution]},
     8: {Liberals: 5, Fascists: 2, hitlerKnowsFascists: false, board:[Executive_Action.NoAction, Executive_Action.InvestigateLoyalty, Executive_Action.SpecialElection, Executive_Action.Execution, Executive_Action.Execution]},
